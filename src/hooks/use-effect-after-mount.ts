dbedd2c06edd5252e94c0e2f6a1427e304b47cf2
Santyzz0311 <santyg0311@gmail.com>
2025-08-09 12:14:53 -0500

    Feat(hook): create useEffectAfterMount hook
    - Add unit testing

---
+++ src/hooks/use-effect-after-mount.ts
@@ -0,0 +1,16 @@
+import { DependencyList, EffectCallback, useEffect } from 'react'
+import { useIsFirstRender } from './use-is-first-render'
+
+/**
+ * Same as `useEffect`, but the effect will only run after the component has mounted.
+ * This hook is useful for performing side effects that should only happen after the initial render.
+ * @param effect - The effect callback to run after the component has mounted
+ * @param deps - The dependency array for the effect
+ */
+export function useEffectAfterMount (effect: EffectCallback, deps?: DependencyList) {
+  const isFirstRender = useIsFirstRender()
+
+  useEffect(() => {
+    if (!isFirstRender) return effect()
+  }, deps)
+}
