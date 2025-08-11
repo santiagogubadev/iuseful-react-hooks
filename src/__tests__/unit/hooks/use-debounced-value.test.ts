b7dc80182d16abec4dbb019e3d4a9b7a3143b1eb
Santyzz0311 <santyg0311@gmail.com>
2025-08-11 08:48:54 -0500

    Update(hook): useebounced value contract and tests changes
    - Add test case
    - Change second param contract

--- src/__tests__/unit/hooks/use-debounced-value.test.ts
+++ src/__tests__/unit/hooks/use-debounced-value.test.ts
@@ -32,4 +32,28 @@ describe('useDebouncedValue', () => {
     expect(clearSpy).toHaveBeenCalled()
     expect(clearSpy).toHaveBeenCalledTimes(1)
   })
+  it('should not update to provided first value if new value is provided', async () => {
+    const values = {
+      first: 'test-1',
+      second: 'test-2',
+      third: 'test-3',
+    }
+    const delayInMs = 1000
+    const { result, rerender } = renderHook(
+      ({ value, delayInMs }) => useDebouncedValue(value, { delayInMs }),
+      {
+        initialProps: { value: values.first, delayInMs },
+      },
+    )
+
+    rerender({ value: values.second, delayInMs })
+    await act(async () => vi.advanceTimersByTime(delayInMs / 2))
+    rerender({ value: values.third, delayInMs })
+    await act(async () => vi.advanceTimersByTime(delayInMs - 1))
+
+    expect(result.current).toBe(values.first)
+
+    await act(async () => vi.advanceTimersByTime(1))
+    expect(result.current).toBe(values.third)
+  })
 })
--- src/hooks/use-debounced-value.ts
+++ src/hooks/use-debounced-value.ts
@@ -1,12 +1,23 @@
 import { useEffect, useState } from 'react'
 
+interface UseDebouncedValueOptionsParam {
+  /**
+   * The debounce delay in milliseconds.
+   * @default 300
+   */
+  delayInMs?: number
+}
+
 /**
  * A hook that returns a debounced version of the input value.
  * @param value - The value to debounce.
- * @param [delayInMs=300] - The debounce delay in milliseconds. Default is 300ms.
+ * @param options - The options for the debounce behavior.
  * @returns The debounced value.
  */
-export function useDebouncedValue<T>(value: T, delayInMs: number = 300): T {
+export function useDebouncedValue<T>(
+  value: T,
+  { delayInMs = 300 }: UseDebouncedValueOptionsParam = {},
+): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value)
 
   useEffect(() => {
