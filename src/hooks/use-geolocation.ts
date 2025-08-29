ab807ec0d8349ae8aaeb3e58666d44df0b2a5fb6
Santyzz0311 <santyg0311@gmail.com>
2025-08-29 13:07:26 -0500

    Feat(hook): add use geolocation to interact with navigator geolocation api

---
+++ src/hooks/use-geolocation.ts
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+
+interface UseGeolocationReturn {
+  /**
+   * Indicates if the geolocation data is being loaded.
+   */
+  loading: boolean
+  /**
+   * Indicates if there was an error retrieving the geolocation data.
+   */
+  error: GeolocationPositionError | null
+  /**
+   * The geolocation data retrieved from the browser.
+   */
+  data: GeolocationCoordinates | null
+}
+
+/**
+ * A custom hook that retrieves the user's geolocation.
+ * @param options Options for the geolocation request.
+ * @returns An object containing the loading state, error (if any), and the geolocation data.
+ */
+export function useGeolocation(options?: PositionOptions): UseGeolocationReturn {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<GeolocationPositionError | null>(null)
+  const [data, setData] = useState<GeolocationCoordinates | null>(null)
+
+  useEffect(() => {
+    const successHandler = (position: GeolocationPosition) => {
+      setLoading(false)
+      setError(null)
+      setData(position.coords)
+    }
+
+    const errorHandler = (error: GeolocationPositionError) => {
+      setError(error)
+      setLoading(false)
+    }
+
+    navigator.geolocation.getCurrentPosition(successHandler, errorHandler, options)
+
+    const watchId = navigator.geolocation.watchPosition(successHandler, errorHandler, options)
+
+    return () => navigator.geolocation.clearWatch(watchId)
+  }, [options])
+
+  return { loading, error, data }
+}
