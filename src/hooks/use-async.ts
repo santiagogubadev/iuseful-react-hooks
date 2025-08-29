5e75d3a02cbe4f992d48e8e32fe3433d034ef3c7
Santyzz0311 <santyg0311@gmail.com>
2025-08-28 20:50:27 -0500

    Feat(hook): use async to manage async operations

---
+++ src/hooks/use-async.ts
@@ -0,0 +1,47 @@
+import { DependencyList, useCallback, useEffect, useState } from 'react'
+
+interface UseAsyncReturn<TValue, CError> {
+  /**
+   * The result of the async operation.
+   */
+  value: TValue | null
+  /**
+   * The error, if any, that occurred during the async operation.
+   */
+  error: CError | null
+  /**
+   * Whether the async operation is currently loading.
+   */
+  loading: boolean
+}
+
+/**
+ * A custom hook for handling asynchronous operations.
+ * @param cb - The async function to call.
+ * @param dependencies - The dependencies for the useEffect hook.
+ * @returns An object containing the result, error, and loading state.
+ */
+export function useAsync<TValue, CError = Error>(
+  cb: () => Promise<TValue>,
+  dependencies: DependencyList = [],
+): UseAsyncReturn<TValue, CError> {
+  const [value, setValue] = useState<TValue | null>(null)
+  const [error, setError] = useState<CError | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  const execute = useCallback(() => {
+    setLoading(true)
+    setValue(null)
+    setError(null)
+    cb()
+      .then(setValue)
+      .catch(setError)
+      .finally(() => setLoading(false))
+  }, dependencies)
+
+  useEffect(() => {
+    execute()
+  }, [execute])
+
+  return { value, error, loading }
+}
