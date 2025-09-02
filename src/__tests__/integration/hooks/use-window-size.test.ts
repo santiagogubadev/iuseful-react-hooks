d7752805a8d2e2a7d60381c05a401c4ae250d8d0
Santyzz0311 <santyg0311@gmail.com>
2025-09-02 18:45:14 -0500

    Fix: use-window-size coverage

--- src/__tests__/integration/hooks/use-window-size.test.ts
+++ src/__tests__/integration/hooks/use-window-size.test.ts
@@ -1,6 +1,6 @@
 import { assertHook } from '@/__tests__/__tests-utils__/assertions'
 import { useWindowSize, useEventListener } from '@/hooks'
-import { renderHook } from '@testing-library/react'
+import { act, renderHook } from '@testing-library/react'
 
 vi.mock('@/hooks/use-event-listener.ts', { spy: true })
 
@@ -18,5 +18,17 @@ describe('useWindowSize', () => {
   it('should update the window size on resize', () => {
     renderHook(() => useWindowSize())
     expect(useEventListener).toHaveBeenCalledWith('resize', expect.any(Function))
+  })
+
+  it('should updates on window resize', async () => {
+    const { result } = renderHook(() => useWindowSize())
+
+    await act(async () => {
+      window.innerWidth = 800
+      window.innerHeight = 600
+      window.dispatchEvent(new Event('resize'))
+    })
+
+    expect(result.current).toEqual({ width: 800, height: 600 })
   })
 })
